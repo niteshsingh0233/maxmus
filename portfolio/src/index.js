67ce73c9e78f681ef519802f34a3b89675021a95
niteshsingh0233 <niteshsingh0233@gmail.com>
2026-05-07 15:00:44 +0530

    maxmus portfolio commit.

---
+++ portfolio/src/index.js
@@ -0,0 +1,10 @@
+// Not necessary to import React in newer versions.
+
+// Main File.
+
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import "./index.css";
+
+ReactDOM.render(<App />, document.querySelector("#root"));
